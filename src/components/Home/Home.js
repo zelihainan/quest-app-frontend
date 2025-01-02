a6e0432b0a74642be56688f5453358a5a798f197
Zeliha İnan <zelihainann@gmail.com>
2025-01-02 17:26:51 +0300

    Postları homepage'den renderladım

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Post from "../Post/Post";
 
 function Home(){
-    return(
-        <div>
-            Home!!!
-        </div>
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [postList, setPostList] = useState([]);
+    
+    useEffect(() => {
+        fetch("/posts").then(res=>res.json()).then((result) => {
+            setIsLoaded(true);
+            setPostList(result);
+        },
+        (error) => {
+            setIsLoaded(true);
+            setError(error);
+        }
     )
+    }, [])
+    if(error){
+        return <div> Error !!!</div>;
+    }else if(!isLoaded){
+        return <div> Loading...</div>;
+    }else{
+        return(
+            <div className="container">
+            Home!!!
+            
+            {postList.map(post => (
+                <Post title={post.title} text= {post.text}></Post>
+            ))}
+            </div>
+        );
+    }
+
 }
 export default Home;
